26cf3b85ea32bc3c69f90e039979691f0d3c612b
Patil Vaishnavi Dinesh <120010598+Vaishnavip99@users.noreply.github.com>
2024-12-26 18:45:16 +0530

    Add files via upload

---
+++ utils/footer.php
@@ -0,0 +1,33 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>V</strong><span class = "small footerSubtext">aishnavi</span>
+                        <strong>P</strong><span class = "small footerSubtext">atil</span>
+                        </p>
+
+                    <p class = "footerSubtext2">
+                        ... &copy;Event Management System
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                    EMS WEBSITE
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/facebook.png">
+                        <img src = "images/twitter.png">
+                        <img src = "images/googleplus.png">
+                        <img src = "images/youtube.png">
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
